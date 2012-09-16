0807fc7b85ef786c17294d90faa7a78d2db3f564
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-17 00:10:00 +0200

    initial commit of ex9 source

---
+++ exercises/chapter_2/src/ex9/ex9.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Please provide a number between 32 and 255: ";
+    size_t value;
+    cin >> value; // read the value from the user
+    string output = "unsigned value: , as character: `\'\n";
+    output.insert(33, 1, value);
+    cout << output;
+}
