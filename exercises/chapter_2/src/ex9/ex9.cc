6d49ae32f7e4b897dbf16ec7127be48dae7a8d7d
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-17 16:02:26 +0200

    ex9 source update

--- exercises/chapter_2/src/ex9/ex9.cc
+++ exercises/chapter_2/src/ex9/ex9.cc
@@ -7,8 +7,11 @@ int main()
 {
     cout << "Please provide a number between 32 and 255: ";
     size_t value;
-    cin >> value; // read the value from the user
-    string output = "unsigned value:  , as character: `\'\n";
-    output.insert(34, 1, value);
-    cout << output[16];
+    cin >> value; // read the value
+    cout << "unsigned value: " << value <<
+            ", as character: `" << static_cast<char>(value) << "\'\n";
+    
+    char ch = value;
+    cout << "unsigned value: " << static_cast<size_t>(ch) <<
+            ", as character: `" << ch << "\'\n";
 }
