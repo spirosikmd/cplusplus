a264d077606b9fce21c668ed84a859aadd069951
Christian Manteuffel <cm@notagain.de>
2012-09-19 23:50:56 +0200

    line wrap

--- exercises/Chapter2/src/ex14/powerOfTwo.cc
+++ exercises/Chapter2/src/ex14/powerOfTwo.cc
@@ -9,5 +9,7 @@ int main() {
     
     bool powerOfTwo =  x != 0 && (x & (x - 1)) == 0;
     
-    cout << (powerOfTwo ? "The value is an exact power of two" : "The value is not an exact power of two" ) << '\n';   
+    cout << (powerOfTwo ? "The value is an exact power of two" : 
+                          "The value is not an exact power of two" ) 
+                          << '\n';   
 }
