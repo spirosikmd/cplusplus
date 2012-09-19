9a7ac31951c90ac0e41e778c2723fb1578788785
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-19 10:44:54 +0200

    ex11

---
+++ exercises/chapter_2/src/ex11/ex11.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string path_string = getenv("PATH"); // retrieve the value of the
+                                         // env variable PATH
+    cout << "Current PATH: " << path_string << "\n"
+            "What to remove? ";
+    string string_to_remove;
+    cin >> string_to_remove; // read the string to be removed
+    size_t opos = path_string.find(string_to_remove);
+    size_t length = string_to_remove.length();
+    path_string.erase(opos, length);
+    cout << "New PATH: " << path_string << "\n";
+}
