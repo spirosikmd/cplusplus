3c569095b256e28bf51a9b8eb516c2dbb5dcf978
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-20 22:04:16 +0200

    ex11

--- exercises/Chapter2/src/ex11/path.cc
+++ exercises/Chapter2/src/ex11/path.cc
@@ -10,25 +10,25 @@ enum Errors
 
 int main()
 {
-    string pathString = getenv("PATH"); // retrieve the value of the
-                                        // env variable PATH
+    string pathString = getenv("PATH"); // Retrieve the value of the
+                                        // env variable `PATH'.
     cout << "Current PATH: " << pathString << '\n';
     
     cout << "What to remove? ";
     string substring;
-    cin >> substring; // read the string to be removed
+    cin >> substring; // Read the string to be removed.
     
     int opos = pathString.find(substring);
-    // Check if find returned SUBSTRING_NOT_FOUND error.
+    // Check if `find' returned SUBSTRING_NOT_FOUND error.
     // If the substring is not found, then by assigning the length of
-    // the pathString to the opos we ensure that no runtime error will
-    // be produced.
+    // the `pathString' to the `opos' we ensure that no runtime error
+    // will be produced.
     opos == SUBSTRING_NOT_FOUND ? opos = pathString.length() : opos;
     
-    size_t on = substring.length(); // get the number of charactes of
-                                    // the substring
-    pathString.erase(opos, on); // erase the substring of the current
-                                // path string starting at the position
-                                // of the first occurrence
+    size_t on = substring.length(); // Get the number of charactes of
+                                    // the `substring'.
+    pathString.erase(opos, on); // Erase the `substring' of the current
+                                // `pathString' starting at the position
+                                // of the first occurrence.
     cout << "New PATH: " << pathString << '\n';
 }
