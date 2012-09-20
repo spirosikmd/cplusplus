9901d9eda586cafe67a8ed10fbfffd6eb1102cfd
Christian Manteuffel <cm@notagain.de>
2012-09-20 23:59:35 +0200

    line.cc format

--- exercises/Chapter2/src/ex12/line.cc
+++ exercises/Chapter2/src/ex12/line.cc
@@ -5,6 +5,10 @@ using namespace std;
 int main() 
 {
     string str;
+<<<<<<< HEAD
     cout << (getline(cin, str).eof() ? "complete line" : 
+=======
+    cout << (getline(cin, str).eof() ? "complete line" :
+>>>>>>> line.cc format
                                        "incomplete line") << "\n";
 }
