af8ec0502eb598d665792c3a16599777301d5cd8
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-21 14:09:28 +0200

    ex11

--- exercises/Chapter2/src/ex11/path.cc
+++ exercises/Chapter2/src/ex11/path.cc
@@ -3,11 +3,6 @@
 
 using namespace std;
 
-enum Errors
-{
-    SUBSTRING_NOT_FOUND=-1
-};
-
 int main()
 {
     // Retrieve the value of the env variable `PATH'.
@@ -19,15 +14,14 @@ int main()
     cin >> substring; // Read the string to be removed.
     
     // Find the index of the first occurence of `substring' in
-    // `pathString'. It returns -1 if the `substring' is not
-    // contained in `pathString'.
-    int opos = pathString.find(substring);
+    // `pathString'. It returns string::npos if the `substring'
+    // is not contained in `pathString'.
+    size_t opos = pathString.find(substring);
     
-    // Check if `find' returned SUBSTRING_NOT_FOUND error.
-    // If the substring is not found, then by assigning the length of
-    // the `pathString' to the `opos' we ensure that no runtime error
-    // will occur.
-    opos = opos == SUBSTRING_NOT_FOUND ? pathString.length() : opos;
+    // Check if `find' returned string::npos. If the substring is not 
+    // found, then by assigning the length of the `pathString' to the 
+    // `opos' we ensure that no runtime error will occur.
+    opos = opos == string::npos ? pathString.length() : opos;
     
     // Get the number of charactes of the `substring'.
     size_t on = substring.length();
