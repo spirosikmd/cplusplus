628f757e5cbbc7d905b75576e85d4ee59f6f54c7
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-23 00:57:31 +0200

    ex19

---
+++ exercises/Chapter3/src/ex19/numOfLines.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string argument("ok");
+    if (argv[1] && argument == argv[1])
+    {
+        size_t numOfLines = 0;
+        string line;
+        while (getline(cin, line))
+            ++numOfLines;
+        cout << "Number of lines read: " << numOfLines << '\n';
+    }
+    else
+    {
+        size_t numOfLines = 0;
+        while (not cin.eof())
+        {
+            string line;
+            getline(cin, line);
+            ++numOfLines;
+        }
+        cout << "Number of lines read: " << numOfLines << '\n';
+    }
+}
