c898a0771b9b89f8d864d888b88f497ac56a60e8
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-09-27 14:04:10 +0200

    ex18

--- exercises/Chapter3/src/ex18/combinations.cc
+++ exercises/Chapter3/src/ex18/combinations.cc
@@ -5,18 +5,26 @@
 using namespace std;
 
 int main(int argc, char *argv[])
-{ 
+{
     for (size_t rowIdx = 0; rowIdx != pow(2, argc - 1); ++rowIdx)
     {
-        cout << rowIdx + 1 << ":";
-        size_t shifter = 1;
+        cout << rowIdx + 1 << ':';
+        size_t mask = 1;
         for (int argIdx = 1; argIdx != argc; ++argIdx)
         {
             string arg = argv[argIdx];
-            cout << ' ' << setw(arg.length())
-                        << ((rowIdx & shifter) > 0 ? arg : "");
-            shifter <<= 1;
-        }
+            
+            // Ensure that at least `arg length' chars will be printed
+            // This will produce the white space chars if the arg 
+            // should not be shown.
+            cout << ' ' << setw(arg.length());
+            // Show the arg only if the the position that the mask
+            // indicates is greater than 0.
+            cout << ((rowIdx & mask) > 0 ? arg : "");
+            
+            // Shift mask to indicate the next position.
+            mask <<= 1;
+        }        
         cout << '\n';
     }
 }
