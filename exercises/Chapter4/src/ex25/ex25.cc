761fb0acdbfa9a4aec16160e799b7828e31b5744
Spyros Ioameimidis <spyrosikmd@gmail.com>
2012-10-02 23:59:46 +0200

    ex25

---
+++ exercises/Chapter4/src/ex25/ex25.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// void fun(int arg);
+// void fun(char const *arg);
+
+void function(int arg = 0);
+void function(char *arg = "\0");
+
+int main()
+{
+    // fun(static_cast<char*>(0));
+    // fun("\0");
+    function();
+}
+
+// void fun(int arg)
+// {
+//     cout << "called the int\n";
+// }
+// void fun(char const *arg)
+// {
+//     cout << "called the char\n";
+// }
+
+void function(int arg)
+{
+    
+}
+
+void function(char const *arg)
+{
+    
+}
---
+++ exercises/Chapter4/src/ex25/ex25.h
@@ -0,0 +1,6 @@
+#ifndef __EX25_H__
+#define __EX25_H__
+
+
+
+#endif
